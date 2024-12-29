0fded4a8753ff45d9d5cc1b11755c755d69ff303
amirhosein sajjadi <canadavancouver2003@gmail.com>
2024-12-29 17:51:37 +0330

    Add Step

--- src/components/layout/side-menu/VSideMenu.vue
+++ src/components/layout/side-menu/VSideMenu.vue
@@ -1,42 +1,50 @@
 <template>
-    <aside class="col-2 border-end">
-        <nav class="list-group list-group-flush py-2">
-            <VSideMenuItem :to="{ name: 'Dashboard' }">
-                <template #icon>
-                    <i class="bi-border-all"></i>
-                </template>
+<aside class="col-2 border-end">
+    <nav class="list-group list-group-flush py-2">
 
-                {{ $t('Dashboard') }}
-            </VSideMenuItem>
 
-            <VSideMenuItem :to="{ name: 'Users' }">
-                <template #icon>
-                    <i class="bi-people"></i>
-                </template>
+        <VSideMenuItem :to="{ name: 'Dashboard' }">
+            <template #icon>
+                <i class="bi-border-all"></i>
+            </template>
 
-                {{ $t('Users') }}
-            </VSideMenuItem>
+            {{ $t('Dashboard') }}
+        </VSideMenuItem>
 
-            <VSideMenuItem :to="{ name: 'Todos' }">
-                <template #icon>
-                    <i class="bi-list-task"></i>
-                </template>
+        <VSideMenuItem :to="{ name: 'Users' }">
+            <template #icon>
+                <i class="bi-people"></i>
+            </template>
 
-                {{ $t('Todos') }}
-            </VSideMenuItem>
-        </nav>
-    </aside>
+            {{ $t('Users') }}
+        </VSideMenuItem>
+
+        <VSideMenuItem :to="{ name: 'Todos' }">
+            <template #icon>
+                <i class="bi-list-task"></i>
+            </template>
+            <!--وظایف-->
+            {{ $t('Todos') }}
+        </VSideMenuItem>
+        <VSideMenuItem :to="{ name: 'step' }">
+            <template #icon>
+                <i class=""></i>
+            </template>
+            Steps
+        </VSideMenuItem>
+    </nav>
+</aside>
 </template>
 
 <script>
-    // Components
-    import VSideMenuItem from '@/components/layout/side-menu/VSideMenuItem.vue';
+// Components
+import VSideMenuItem from '@/components/layout/side-menu/VSideMenuItem.vue';
 
-    export default {
-        name: 'VSideBar',
+export default {
+    name: 'VSideBar',
 
-        components: {
-            VSideMenuItem
-        }
+    components: {
+        VSideMenuItem
     }
+}
 </script>
